32cf28750729a60399fc5daaca175f602398d259
Diana <webmaster@dan.funvic.ru>
2019-06-08 19:24:09 +0300

    Добавлен роутинг для страницы с qr

--- resources/views/cabinet/ticket.blade.php
+++ resources/views/cabinet/ticket.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 	<div class="param-qr">
-		<div class="qr-code"><img src="http://chart.apis.google.com/chart?cht=qr&chs=320x320&chl=3ihdhg4hg34vr3g434r"></div>
+		<div class="qr-code"><img src="blank.png" alt="3ihdhg4hg34vr3g434r"></div>
 	</div>
 	<div class="param-visual">
-		<div class="ticket-time"><?=date("d.m.Y H:i", 1560006691) ?></div>//$ticket_data['timestamp']
+		<div class="ticket-time"><?=date("d.m.Y H:i", 1560006691) ?></div>
 		<div class="ticket-bus"><span>Автобус</span> <span>№34</span> - <span>31 </span><span>{{ trans('dictionary.rub') }}</span></div>
 	</div>
 	<p>{{ trans('dictionary.ticket_show_info') }}</p>
